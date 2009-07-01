23bfe3d57c90adc18d4dd7447de1e08dadddeb44
Hillary Johnson <xoxiety@gmail.com>
2009-06-30 21:01:11 -0700

    fixing blog show

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,7 @@
 class Blog < ActiveRecord::Base
   validates_presence_of :body, :title, :author
   has_many :comments
-  belongs_to :page
-  belongs_to :parent
+  
   
   def self.search(search, page)
     paginate :per_page => 5, :page => page, :conditions => ['body like ?', "%#{search}%"], :order => 'title'
